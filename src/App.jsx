0275f18edbcd584919b638faac55108c562f1105
Sekkappan Vinaitheerthan <kathir0516@gmail.com>
2026-01-07 22:46:17 +0530

    Added React Router and used it to make seperate pages for components, added /cart and /home routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
-import CoffeeList from "./components/CoffeeList";
-import Cart from "./components/Cart.jsx"
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home.jsx";
+import CartPage from "./pages/CartPage.jsx";
 
 
 export default function App() {
@@ -8,8 +9,12 @@ export default function App() {
   return (
     <div className="min-h-screen bg-[#f5f5f5] p-6">
       <Navbar />
-      <CoffeeList />
-      <Cart />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useCart } from "../context/CartContext";
+import { Link } from "react-router";
 
 export default function Navbar() {
   const { cart, clearCart } = useCart();
@@ -7,9 +8,9 @@ export default function Navbar() {
 
   return (
     <nav className="bg-[#3e2723] text-white p-4 flex justify-between">
-      <h1 className="text-xl font-bold">☕ Coffee Store</h1>
+      <Link to="/" className="text-xl font-bold">☕ Coffee Store</Link>
       <div className="flex items-center gap-4">
-        <span>Cart ({cartCount})</span>
+        <Link to="/cart">Cart ({cartCount})</Link>
         <button onClick={clearCart}
           className="bg-red-500 px-3 py-1 rounded text-sm hover:bg-red-600"
           disabled={cartCount === 0}>
